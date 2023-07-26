9a8710904bb9d81bd00ec50a70d9412a8639ef5d
jfuentdi <98829721+jfuentdi@users.noreply.github.com>
2023-07-26 13:38:26 -0400

    Update settings.py

--- DemoFaceAPI/DemoFaceAPI/settings.py
+++ DemoFaceAPI/DemoFaceAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-50iz74h^6d4a_2h=p=ib3&^)649g%$wiea@0!eer)@)4azj5r-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['chls1zu1apparqpoccomm001.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
